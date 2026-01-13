456bf458f8bbbd23d58c348a61517ef6ae05385d
Reggie Bowers <bowersrd@goldmail.etsu.edu>
2026-01-13 14:10:04 -0500

    Updates

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import { MotionPlugin } from '@vueuse/motion'
-
-const app = createApp(App)
-
-app.use(MotionPlugin)
-app.mount('#app')
